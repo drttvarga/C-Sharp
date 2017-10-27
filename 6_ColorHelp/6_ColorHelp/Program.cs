36dd479e9668f6d05a2a566b8ab993d62ea9c4d8
Varga Dorottya <drtt.varga@gmail.com>
2017-10-27 11:22:13 +0200

    add ColorHelp

---
+++ 6_ColorHelp/6_ColorHelp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_ColorHelp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string g = "green";
+            Console.WriteLine("Itsy bitsy spider " + g);
+            Console.ForegroundColor = ConsoleColor.Red;
+            string r = "red";
+            Console.WriteLine("Itsy bitsy spider " + r);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string y = "yellow";
+            Console.WriteLine("Itsy bitsy spider " + y);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string b = "blue";
+            Console.WriteLine("Itsy bitsy spider " + b);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            string m = "magenta";
+            Console.WriteLine("Itsy bitsy spider " + m);
+            Console.ReadKey();
+        }
+    }
+}
