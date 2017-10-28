1ebbe9df3dd4c252a6c8c4dfcb51184fd0c62787
Varga Dorottya <drtt.varga@gmail.com>
2017-10-28 17:27:52 +0200

    Update

---
+++ 7_ScreenFrame/7_ScreenFrame/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ScreenFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(20, 10);
+            Console.SetCursorPosition(0, 0);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 1);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 2);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 3);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 4);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 5);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 6);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 7);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 8);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 9);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 10));
+            Console.Write("*");
+            Console.SetCursorPosition(0, 11);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 12);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 13);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 14);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 15);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 16);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 17);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 18);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 19);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 20);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(1, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(2, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(3, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(4, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(5, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(6, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(7, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(8, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(9, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(10, 0);
+            Console.WriteLine("*");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8_Smiley/8_Smiley/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Smiley
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eyes
+            Console.SetCursorPosition(1, 0);
+            Console.Write("*");
+            Console.SetCursorPosition(3, 0);
+            Console.WriteLine("*");
+            //Mouth
+            Console.WriteLine("*");
+            Console.SetCursorPosition(1, 2);
+            Console.WriteLine("***");
+            Console.SetCursorPosition(4, 1);
+            Console.WriteLine("*");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Eyes
+            Console.SetCursorPosition(2, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(2, 1);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(4, 0);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(4, 1);
+            Console.WriteLine("*");
+            //Mouth
+            Console.WriteLine("*"); //cursor position (2, 1)
+            Console.SetCursorPosition(1, 3);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("***");
+            Console.SetCursorPosition(6, 2);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(5, 3);
+            Console.WriteLine("*");
+
+            Console.ReadKey();
+
+        }
+    }
+}
