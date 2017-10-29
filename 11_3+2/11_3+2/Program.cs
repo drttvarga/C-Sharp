b4ced977f774409bbfde67518d43d3413a163ad4
Varga Dorottya <drtt.varga@gmail.com>
2017-10-29 14:38:08 +0100

    #11

---
+++ 11_3+2/11_3+2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3+2={0}", 3 + 2);
+            Console.ReadKey();
+
+            //Also works
+            /* Console.Write("3 + 2 = ");
+             Console.Write(3 + 2);
+             Console.ReadKey();*/
+        }
+    }
+}
