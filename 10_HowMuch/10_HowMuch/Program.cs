71166a3d1c57dcc6c1b9902600d3a133cf857887
Varga Dorottya <drtt.varga@gmail.com>
2017-10-29 14:27:16 +0100

    #10

---
+++ 10_HowMuch/10_HowMuch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_HowMuch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3+2");//stringként kezeli
+            Console.WriteLine(3 + 2);//összeadás
+            Console.WriteLine(3 * 2);//szorzás
+            Console.WriteLine(3 - 2);//kivonás
+            Console.WriteLine(3 / 2);//osztás
+            Console.WriteLine(17 % 3);//osztás után a maradékot írja ki
+            Console.ReadKey();
+        }
+    }
+}
