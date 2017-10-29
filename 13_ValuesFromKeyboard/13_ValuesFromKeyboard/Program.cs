bff63cd75e9a5a0e7334566e56d183cd27a15913
Varga Dorottya <drtt.varga@gmail.com>
2017-10-29 15:55:44 +0100

    #13

---
+++ 13_ValuesFromKeyboard/13_ValuesFromKeyboard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ValuesFromKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First number
+            Console.Write("Enter the first number: ");
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            //Second number
+            Console.Write("Enter the second number: ");
+            string p = Console.ReadLine();
+            int b = int.Parse(p);
+            // calculations
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+            Console.WriteLine("{2}-{1}={0}", a, b, a + b);
+            Console.ReadKey();
+
+            //Counting with fractional numbers
+            //First number
+            Console.Write("Enter the first number: ");
+            string z = Console.ReadLine();
+            double c = double.Parse(z);
+            //Second number
+            Console.Write("Enter the second number: ");
+            string r = Console.ReadLine();
+            double d = double.Parse(r);
+            // calculations
+            Console.WriteLine("{0}+{1}={2}", c, d, c + d);
+            Console.WriteLine("{2}-{1}={0}", c, d, c + d);
+            Console.ReadKey();
+        }
+    }
+}
