05a73daba99ec2cb75ecc933807652ae9d41716a
Varga Dorottya <drtt.varga@gmail.com>
2017-10-30 08:12:32 +0100

    #17

---
+++ 17_Celsius-Fahrenheit/17_Celsius-Fahrenheit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Celsius_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a temperature value in degree Celsius: ");
+            string c = Console.ReadLine();
+            double celsius = double.Parse(c);
+            //Celsius to Fahrenheit: F = C * 1.8 + 32
+            double fahrenheit = celsius * 1.8 + 32;
+            Console.WriteLine("The temperature in Fahrenheit: {3}", celsius, 1.8, 32, fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
