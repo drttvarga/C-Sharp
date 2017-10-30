5328b804069ad3337bce96cafbb48dd9efbd5081
Varga Dorottya <drtt.varga@gmail.com>
2017-10-30 12:40:34 +0100

    #14

---
+++ 14_Digit_request_v1/14_Digit_request_v1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Digit_request_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", s);
+            Console.Write("Enter the second number: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", r);
+            int t = (s * 10 + r) * 2;
+            Console.WriteLine("Your number is: {0}", t);
+
+            Console.ReadKey();
+        }
+    }
+}
