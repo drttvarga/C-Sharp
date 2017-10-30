449c6c9d89ae08f434b6fd458253ac61a83ba2d1
Varga Dorottya <drtt.varga@gmail.com>
2017-10-30 11:09:42 +0100

    #16

---
+++ 16_All_operations/16_All_operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_All_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            Console.Write("Enter the second number: ");
+            string r = Console.ReadLine();
+            int b = int.Parse(r);
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+            Console.ReadKey();
+        }
+    }
+}
