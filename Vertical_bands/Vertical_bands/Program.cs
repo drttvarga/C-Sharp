d59cb9ac1b385184e861728beae72f1f0c4effc4
Varga Dorottya <drtt.varga@gmail.com>
2017-11-04 17:42:20 +0100

    #2 függőleges sávok

---
+++ Vertical_bands/Vertical_bands/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Vertical_bands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Thread.Sleep(1000);
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Thread.Sleep(1000);
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Thread.Sleep(1000);
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.Write("* ");
+            Console.WriteLine("*");
+            Console.ReadKey();
+
+        }
+    }
+}
