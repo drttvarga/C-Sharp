4ae5f4535c012e288c4db183666b751d4f261443
Varga Dorottya <drtt.varga@gmail.com>
2017-11-04 16:48:56 +0100

    multiplication table

---
+++ Multiplication_table/Multiplication_table/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("+--------------------------------------+");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 1   2   3   4   5   6   7   8   9  10");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 2");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("   4  6   8  10  12  14  16   18  20");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 3");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("   6   9  12  15  18  21  24  27  30");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 4");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("   8  12  16  20  24  28  32  36  40");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 5");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  10  15  20  25  30  35  40  45  50");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 6");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  12  18  24  30  36  42  48  54  60");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 7");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  14  21  28  35  42  49  56  63  70");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 8");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  16  24  32  40  48  56  64  72  80");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" 9");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  18  27  36  45  54  63  72  81  90");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("10");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("  20  30  40  50  60  70  80  90 100");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("|");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("+--------------------------------------+");
+
+            Console.ReadKey();
+            
+           
+        }
+    }
+}
