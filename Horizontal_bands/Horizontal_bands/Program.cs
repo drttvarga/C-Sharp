94926a83b9c28a52fd2ce6465283b6c35afa705e
Varga Dorottya <drtt.varga@gmail.com>
2017-11-04 17:15:37 +0100

    #1 vízszintes sávok

---
+++ Horizontal_bands/Horizontal_bands/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Horizontal_bands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("********************");
+            Console.WriteLine("********************");
+            Thread.Sleep(1000);
+            Console.WriteLine("********************");
+            Console.WriteLine("********************");
+            Thread.Sleep(1000);
+            Console.WriteLine("********************");
+            Console.WriteLine("********************");
+            Thread.Sleep(1000);
+            Console.WriteLine("********************");
+            Console.WriteLine("********************");
+            Console.ReadKey();
+        }
+    }
+}
